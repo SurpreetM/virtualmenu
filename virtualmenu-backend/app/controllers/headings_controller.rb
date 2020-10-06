32bea906971fed57cf5c6d8a9a00ba5a216829a1
Surpreet Mundra <surpreet.mundra@gmail.com>
2020-10-06 10:14:36 -0400

    adding the backend heading create action in rails

--- virtualmenu-backend/app/controllers/headings_controller.rb
+++ virtualmenu-backend/app/controllers/headings_controller.rb
@@ -1,6 +1,20 @@
 class HeadingsController < ApplicationController
+
     def index
         headings = Heading.all
         render json: HeadingSerializer.new(headings)
     end
+
+    def create
+        heading = Heading.new
+        heading.name = params[:heading][:name]
+        heading.save
+        # Not sure if we need this
+        render json: HeadingSerializer.new(heading)
+    end 
+
+    def destroy
+    end
+
+
 end
--- virtualmenu-backend/app/models/heading.rb
+++ virtualmenu-backend/app/models/heading.rb
@@ -1,3 +1,4 @@
 class Heading < ApplicationRecord
+    validates :name, presence: true 
     has_many :food_items
 end
